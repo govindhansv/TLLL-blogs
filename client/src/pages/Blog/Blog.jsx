051c4cd09e0e6489b19be9d9522c2fc9486616d4
govindhansv <govindhansv333@gmail.com>
2022-01-22 21:00:46 +0530

    most versions completed

--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Profileblock from '../../components/Profileblock/Profileblock';
+// import Profileblock from '../../components/Profileblock/Profileblock';
 import Singlepost from '../../components/Singlepost/Singlepost';
 import './Blog.css'
 
@@ -11,7 +11,7 @@ import './Blog.css'
       <>
         <div>
           <Singlepost />
-          <Profileblock />
+          {/* <Profileblock /> */}
         </div>
       </>
     )
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -4,47 +4,15 @@ import { useNavigate } from 'react-router-dom';
 const Profile = () => {
 
   let user = JSON.parse(localStorage.getItem('user'))
-  const [username, setusername] = useState(user.username);
-  const [gmail, setgmail] = useState(user.gmail);
-  const [password, setpassword] = useState('');
-  const [id, setid] = useState(user._id);
-
   const navigate = useNavigate()
-
-
-  const PostData = () => {
-
-
-    fetch("users/update", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username,
-        gmail,
-        password, id
-      })
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data);
-        let user = JSON.parse(localStorage.getItem('user'))
-        console.log(user);
-        user.username = username
-        user.gmail = gmail
-        user.password = password
-        localStorage.setItem('user', JSON.stringify(user))
-        console.log(JSON.parse(localStorage.getItem('user')));
-        navigate('/profile')
-
-      })
-  }
+ 
 
   return <div>
     <main class="profile-page">
       <section class="relative block h-500-px">
         <div class="absolute top-0 w-full h-full bg-center bg-cover" >
           <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
+          <img src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg " alt="" />
         </div>
         <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" >
           <svg class="absolute bottom-0 overflow-hidden" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
@@ -64,12 +32,16 @@ const Profile = () => {
                 </div>
                 <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                   <div class="py-6 px-3 mt-32 sm:mt-0">
-                    <button class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+                    {/* <button class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
                       Follow
-                    </button>
+                    </button> */}
+                    <button className=" bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" onClick={()=>{navigate('/edit-profile')}} >Edit Profile</button>
+                    <button className=" bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" onClick={()=>{navigate('/myblogs')}} >Myblogs</button>
+                    <button className=" bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" onClick={()=>{navigate('/newblog')}} >New +</button>
+
                   </div>
                 </div>
-                <div class="w-full lg:w-4/12 px-4 lg:order-1">
+                {/* <div class="w-full lg:w-4/12 px-4 lg:order-1">
                   <div class="flex justify-center py-4 lg:pt-4 pt-8">
                     <div class="mr-4 p-3 text-center">
                       <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span class="text-sm text-blueGray-400">Friends</span>
@@ -81,7 +53,7 @@ const Profile = () => {
                       <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span class="text-sm text-blueGray-400">Comments</span>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
               <div class="text-center mt-12">
                 <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
@@ -91,79 +63,34 @@ const Profile = () => {
                   <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
                   {user.gmail}
                 </div>
-                <div class="mb-2 text-blueGray-600 mt-10">
+                {/* <div class="mb-2 text-blueGray-600 mt-10">
                   <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Solution Manager - Creative Tim Officer
                 </div>
                 <div class="mb-2 text-blueGray-600">
                   <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>University of Computer Science
-                </div>
+                </div> */}
               </div>
               <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
                 <div class="flex flex-wrap justify-center">
                   <div class="w-full lg:w-9/12 px-4">
-                    <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    {/* <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
                       An artist of considerable range, Jenna the name taken by
                       Melbourne-raised, Brooklyn-based Nick Murphy writes,
                       performs and records all of his own music, giving it a
                       warm, intimate feel with a solid groove structure. An
                       artist of considerable range.
-                    </p>
-                    <a href="#pablo" class="font-normal text-pink-500">Show more</a>
+                    </p> */}
+                    {/* <a href="#pablo" class="font-normal text-pink-500">Show more</a> */}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <footer class="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-          <div class="container mx-auto px-4">
-            <div class="flex flex-wrap items-center md:justify-between justify-center">
-              <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-                <div class="text-sm text-blueGray-500 font-semibold py-1">
-                  Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+
       </section>
     </main>
-    {/* <div className="settings">
-      <div className="settingsWrapper">
-        <div className="settingsTitle">
-          <span className="settingsTitleUpdate">Update Your Account</span>
-          <button className="settingsTitleDelete">Delete Account</button>
-        </div>
-        <div className="settingsForm">
-          <label>Profile Picture</label>
-          <div className="settingsPP">
-            <img
-              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
-            <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
-            </label>
-            <input
-              id="fileInput"
-              type="file"
-              style={{ display: "none" }}
-              className="settingsPPInput"
-            />
-          </div>
-          <label>Username</label>
-          <input type="text" value={username} onChange={(e) => { setusername(e.target.value) }} placeholder="eg: Jhon " name="name" />
-          <label>Email</label>
-          <input type="email" value={gmail}  onChange={(e) => { setgmail(e.target.value) }} placeholder="safak@gmail.com" name="email" />
-          <label>Password</label>
-          <input type="password" value={password} onChange={(e) => { setpassword(e.target.value) }} placeholder="Type new Password" name="password" />
-          <button className="settingsSubmitButton" onClick={()=>{PostData()}}>
-            Update
-          </button>
-          
-        </div>
-      </div>
-    </div> */}
+    
   </div>;
 };
 
