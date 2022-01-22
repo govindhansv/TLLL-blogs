115d5c4cee68a6ec8ce56d814b46916d54917bda
Gokul Hans V <gokulhansv@gmail.com>
2022-01-22 14:19:08 +0530

    @design@

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,7 +6,76 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <div>
-            <footer className="page-footer black">
+
+<footer class="page-footer font-small blue pt-4">
+
+  <div class="container-fluid text-center text-md-left">
+
+    <div class="row">
+
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <h5 class="text-uppercase">Footer Content</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+
+      <div class="clearfix w-100 d-md-none pb-3">
+
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+    </div>
+
+  </div>
+
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+
+</footer>
+
+            {/* <footer className="page-footer black">
                 <div className="container">
                     <div className="row">
                         <div className="col l6 s12">
@@ -34,7 +103,7 @@ const Footer = () => {
                         <a className="grey-text text-lighten-4 right" href="/">More Links</a>
                     </div>
                 </div>
-            </footer>
+            </footer> */}
         </div>
     );
 };
--- client/src/pages/Forms/Newblog/Newblog.jsx
+++ client/src/pages/Forms/Newblog/Newblog.jsx
@@ -4,43 +4,71 @@ import './Newblog.css'
 
  
 const Newblog = () => {
-const navigate = useNavigate()
 
-let user = JSON.parse(localStorage.getItem('user'))
-console.log(user);
+// const navigate = useNavigate()
 
-  const [title, settitle] = useState('');
-  const [blog, setblog] = useState('');
-  const [img, setimg] = useState('');
-  var userid = user._id
-  var author = user.username
-  var id
-  const PostData = () => {
-    console.log('s');
-    fetch("users/newblog", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        title, blog,img,userid,author
-      })
-    }).then(response => response.json())
-      .then(data => {
-        localStorage.setItem('myblog',JSON.stringify(data.blogdata))
-        console.log(data.blogdata.title);
-        id = data.blogdata.title
-        navigate(`/blog/${id}`)
-      })
-  }
+// let user = JSON.parse(localStorage.getItem('user'))
+// console.log(user);
+
+//   const [title, settitle] = useState('');
+//   const [blog, setblog] = useState('');
+//   const [img, setimg] = useState('');
+//   var userid = user._id
+//   var author = user.username
+//   var id
+//   const PostData = () => {
+//     console.log('s');
+//     fetch("users/newblog", {
+//       method: "post",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         title, blog,img,userid,author
+//       })
+//     }).then(response => response.json())
+//       .then(data => {
+//         localStorage.setItem('myblog',JSON.stringify(data.blogdata))
+//         console.log(data.blogdata.title);
+//         id = data.blogdata.title
+//         navigate(`/blog/${id}`)
+//       })
+//   }
 
   return <div>
-    <div className="write">
-      {/* <img
+    
+<form>
+  <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
+    <div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+      <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">ADD POST</h1>
+      <div className="space-y-4">
+        <div>
+          <label for="title" className="text-lx font-serif">Title:</label>
+          <input type="text" placeholder="title" id="title" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+        </div>
+        <div>
+          <label for="description" className="block mb-2 text-lg font-serif">Description:</label>
+          <textarea id="description" cols="30" rows="10" placeholder="whrite here.." className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"></textarea>
+        </div>
+        <div>
+          <label for="name" className="text-lx font-serif">Name:</label>
+          <input type="text" placeholder="name" id="name" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+        </div>
+        <div>
+          <label for="email" className="text-lx font-serif">Email:</label>
+          <input type="text" placeholder="name" id="email" className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md" />
+        </div>
+        <button className=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">ADD POST</button>
+      </div>
+    </div>
+  </div>
+</form>
+    {/* <div className="write">
+      <img
         className="writeImg"
         src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
-      /> */}
+      />
 
       <div className="writeForm">
         <div className="writeFormGroup">
@@ -78,7 +106,7 @@ console.log(user);
         </div>
         <button className="writeSubmit"  onClick={()=>{PostData()}}> Publish </button>
       </div>
-    </div>
+    </div> */}
   </div>;
 };
 
